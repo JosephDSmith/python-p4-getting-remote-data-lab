31939913cc59b38e04aea4bdddba298acde121b3
Joseph Smith <joesmith40@gmail.com>
2023-06-16 22:32:41 -0500

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data_list = []
+        data = json.loads(self.get_response_body())
+        for d in data:
+            data_list.append(d) 
+
+        return data_list
